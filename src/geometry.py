0483d7714ef80e675802136ee50174d1441f92d4
Steeven Flahaut <steeven.flahaut.etu@univ-lille.fr>
2024-01-24 10:37:58 +0100

    fonction faites + test vert

--- src/geometry.py
+++ src/geometry.py
@@ -1,1 +1,15 @@
 import math
+from typing import Tuple
+
+
+##class Geometry:
+def calculate_vector(coord_1: Tuple, coord_2: Tuple):
+    """
+            Calculate Vector between two points in 2D space.
+
+            :param coord_1: Tuple -- (x, y) coords of first point
+            :param coord_2: Tuple -- (x, y) coords of second p
+            :return: Tuple -- Vector between two points
+        """
+    vector = (coord_2[0] - coord_1[0], coord_2[1] - coord_1[1])
+    return vector
--- test/test_geometry.py
+++ test/test_geometry.py
@@ -1,16 +1,18 @@
 import unittest
 
+from src.geometry import calculate_vector
+
 
 class MyTestCase(unittest.TestCase):
-    def test_distance(self):
+    def test_calculate_vector(self):
         coord_1 = (0, 0)
         coord_2 = (2, 2)
-        self.assertEqual(distance(coord_1,coord_2), (2, 2))
+        result_1 = (2, 2)
+        self.assertEqual(calculate_vector(coord_1, coord_2), result_1)
 
         coord_3 = (-3, 6)
         coord_4 = (4, -1)
-        self.assertEqual(distance(coord_3, coord_4), (7, -7))
-
+        self.assertEqual(calculate_vector(coord_3, coord_4), (7, -7))
 
 
 if __name__ == '__main__':
