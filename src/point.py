c8b6530829dd98a06a7066f92f9c4accabcbc5ae
Steeven Flahaut <steeven.flahaut.etu@univ-lille.fr>
2024-01-31 21:32:12 +0100

    test move vert

--- src/point.py
+++ src/point.py
@@ -6,5 +6,9 @@ class Point:
         self.x = x
         self.y = y
 
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
     def distance(self, other_point):
         return math.sqrt((other_point.x - self.x) ** 2 + (other_point.y - self.y) ** 2)
