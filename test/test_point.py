57e7d86b4a99199336a9f26550a4ac0f81f4d21c
Steeven Flahaut <steeven.flahaut.etu@univ-lille.fr>
2024-01-31 21:19:51 +0100

    Creation de la classe test_point + test getx gety et Distance

---
+++ test/test_point.py
@@ -0,0 +1,24 @@
+import unittest
+import math
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.p1 = Point(7.0, 4.0)
+        self.p2 = Point(3.0, 8.0)
+
+    # end of initialisation
+    def test_getX(self):
+        self.assertEqual(self.p1.x, 7.0)
+        self.assertEqual(self.p2.x, 3.0)
+
+    def test_getY(self):
+        self.assertEqual(self.p1.y, 4.0)
+        self.assertEqual(self.p2.y, 8.0)
+
+    def test_distance(self):
+        self.assertEqual(self.p1.distance(self.p2), math.sqrt(32))
+        self.assertEqual(self.p2.distance(self.p1), math.sqrt(32))
+
+
+if __name__ == '__main__':
+    unittest.main()
