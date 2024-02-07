a7fc7777fb510ebff0fb752a22b16d80a8e37a7b
Steeven Flahaut <steeven.flahaut.etu@univ-lille.fr>
2024-02-07 08:41:33 +0100

    modification add_point => test vert si deux points sont egaux

--- src/polygon.py
+++ src/polygon.py
@@ -8,8 +8,11 @@ class Polygon:
         return self.points
 
     def add_points(self, points_list):
+        same_point_in_list = []
         for point in points_list:
-            if point not in self.get_points():
+            for polygon_point in self.points:
+                same_point_in_list.append(point.equals_to(polygon_point))
+            if True not in same_point_in_list:
                 self.points.append(point)
 
     def supp_points(self, points_list):
