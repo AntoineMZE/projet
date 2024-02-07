ccc4ed6eadcc43de1363681dfaff070d89ab02b4
antoine <antoine.mazure.etu@univ-lille.fr>
2024-02-07 09:56:36 +0100

    Ajout documentation simple dans point et polygon

--- src/polygon.py
+++ src/polygon.py
@@ -5,9 +5,11 @@ class Polygon:
         self.length = len(points)
 
     def get_points(self):
+        # Renvoie la liste de points du polygon
         return self.points
 
     def add_points(self, points_list):
+        # Ajoute un ou plusieurs points dans la liste déjà existante si ces derniers n'y sont pas déjà
         same_point_in_list = []
         for point in points_list:
             for polygon_point in self.points:
@@ -16,6 +18,7 @@ class Polygon:
                 self.points.append(point)
 
     def supp_points(self, points_list):
+        # Supprime un ou plusieurs dans la liste déjà existante si ces derniers existent dans la liste
         for point in points_list:
             if point in self.get_points():
                 self.points.remove(point)
